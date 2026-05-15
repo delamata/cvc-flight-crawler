b2a1b20e7cec561adf9377b096567b88d2a795dc
delamata <andre.delamata@gmail.com>
2026-05-15 18:14:47 -0300

    fix: make ConectaAS env parsing tolerant

--- crawler/config.py
+++ crawler/config.py
@@ -17,10 +17,10 @@ class Settings(BaseSettings):
     api_secret_key: str = Field(default="troque_esta_chave_em_producao", alias="API_SECRET_KEY")
 
     conectaas_url: str = Field(default="", alias="CONECTAAS_URL")
-    conectaas_token: str | None = Field(default=None, alias="CONECTAAS_TOKEN")
+    conectaas_token: str = Field(default="", alias="CONECTAAS_TOKEN")
     conectaas_pax: str = Field(default="40", alias="CONECTAAS_PAX")
-    conectaas_max_results: int = Field(default=100, alias="CONECTAAS_MAX_RESULTS")
-    conectaas_max_number_of_stops: int = Field(default=1, alias="CONECTAAS_MAX_NUMBER_OF_STOPS")
+    conectaas_max_results: str = Field(default="100", alias="CONECTAAS_MAX_RESULTS")
+    conectaas_max_number_of_stops: str = Field(default="1", alias="CONECTAAS_MAX_NUMBER_OF_STOPS")
     conectaas_routes: str = Field(default="50", alias="CONECTAAS_ROUTES")
     conectaas_business_class: str = Field(default="ALSO", alias="CONECTAAS_BUSINESS_CLASS")
 
@@ -28,6 +28,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         populate_by_name = True
+        extra = "ignore"
 
 
 @lru_cache
